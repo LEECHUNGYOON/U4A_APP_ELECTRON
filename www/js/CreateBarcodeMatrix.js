f443a8d59ed92fe5d482bb0324e73f3cb5f1a7ca
LEECHUNGYOON <36251628+LEECHUNGYOON@users.noreply.github.com>
2022-02-21 15:27:04 +0900

    오후 3:27 2022-02-21
    오후 3:27 2022-02-21

--- www/js/CreateBarcodeMatrix.js
+++ www/js/CreateBarcodeMatrix.js
@@ -9,106 +9,117 @@
 			properties: ['openFile', 'openDirectory', 'multiSelections']
 		});
 
-		//폴더 지정 않하면 종료 
-		if (typeof Tpath === "undefined") {
-			return;
-		}
+		Tpath.then(function (oPaths) {
 
-		var Spath = Tpath[0],
-			loadUrl = `file://${__dirname}/` + `CreateBarcodeMatrix.html`;
-		var sOption = {
-			width: 389,
-			height: 150,
-			resizable: false,
-			movable: false,
-			closable: true,
-			frame: false
-		};
+			var sFolderPath = oPaths.filePaths[0];
 
-
-		//병렬 처리에 대한 결과리턴 이벤트 - 이벤트가 존재하지않을경우만 생성한다 
-		if (!oAPP.findIPCEventName(oAPP.ipcMain.eventNames(), 'if-barcodeMTXdataCB')) {
-
-			oAPP.ipcMain.on('if-barcodeMTXdataCB', (event, res) => {
-
-				var cnt = oAPP.onCreateBarcodeMatrix.actCnt;
-				cnt = cnt - 1;
-				oAPP.onCreateBarcodeMatrix.actCnt = cnt;
-
-
-				if (!oAPP.onCreateBarcodeMatrix.res) {
-					oAPP.onCreateBarcodeMatrix.res = [];
-				}
-
-				oAPP.onCreateBarcodeMatrix.res = oAPP.onCreateBarcodeMatrix.res.concat(res);
-
-				if (cnt > 0) {
-					return;
-				}
-
-				delete oAPP.onCreateBarcodeMatrix.actCnt;
-
-				var oFrame = document.getElementById('u4aAppiFrame');
-
-				var SendData = {
-					REQCD: 'CreateBarcodeMatrix',
-					RESP: oAPP.onCreateBarcodeMatrix.res
-				};
-				delete oAPP.onCreateBarcodeMatrix.res;
-
-				oFrame.contentWindow.postMessage(SendData, '*');
-
-			});
-
-		}
-
-		//3보다 작은경우 바로 전송.
-		if (d.length <= 3) {
-
-			var aSendData = {};
-			aSendData.PATH = Spath;
-			aSendData.DATA = d;
-
-			oAPP.onCreateBarcodeMatrix.actCnt = 1;
-			oAPP.openModalWindow(loadUrl, sOption, aSendData);
-
-			return;
-		}
-
-
-		var rs = d.length % 3,
-			st = 0,
-			ed = Math.floor(d.length / 3),
-			ct = ed;
-
-		//병렬 수행 카운트 설정 
-		oAPP.onCreateBarcodeMatrix.actCnt = 3;
-
-		for (var i = 1, l = 3; i <= l; i++) {
-
-			//마지막인경우 나머지값을 더해줌.
-			if (i === l) {
-				ed += rs;
+			//폴더 지정 않하면 종료 
+			if (typeof sFolderPath == "undefined") {
+				return;
 			}
 
-			var aSendData = {};
-			aSendData.PATH = Spath;
-			aSendData.DATA = d.slice(st, ed);
-			oAPP.openModalWindow(loadUrl, sOption, aSendData);
+			var Spath = sFolderPath,
+				loadUrl = `file://${__dirname}/` + `CreateBarcodeMatrix.html`;
+			var sOption = {
+				width: 389,
+				height: 150,
+				resizable: false,
+				movable: false,
+				closable: true,
+				frame: false
+			};
 
-			//호출 이후 exit.
-			if (i === l) {
-				break;
+			//병렬 처리에 대한 결과리턴 이벤트 - 이벤트가 존재하지않을경우만 생성한다 
+			if (!oAPP.findIPCEventName(oAPP.ipcMain.eventNames(), 'if-barcodeMTXdataCB')) {
+
+				oAPP.ipcMain.on('if-barcodeMTXdataCB', (event, res) => {
+
+					var cnt = oAPP.onCreateBarcodeMatrix.actCnt;
+					cnt = cnt - 1;
+					oAPP.onCreateBarcodeMatrix.actCnt = cnt;
+
+
+					if (!oAPP.onCreateBarcodeMatrix.res) {
+						oAPP.onCreateBarcodeMatrix.res = [];
+					}
+
+					oAPP.onCreateBarcodeMatrix.res = oAPP.onCreateBarcodeMatrix.res.concat(res);
+
+					if (cnt > 0) {
+						return;
+					}
+
+					delete oAPP.onCreateBarcodeMatrix.actCnt;
+
+					var oFrame = document.getElementById('u4aAppiFrame');
+
+					var SendData = {
+						REQCD: 'CreateBarcodeMatrix',
+						RESP: oAPP.onCreateBarcodeMatrix.res
+					};
+					delete oAPP.onCreateBarcodeMatrix.res;
+
+					oFrame.contentWindow.postMessage(SendData, '*');
+
+				});
+
 			}
 
-			//종료값을 시작값으로 옮김.
-			st = ed;
+			//3보다 작은경우 바로 전송.
+			if (d.length <= 3) {
 
-			//종료값 +
-			ed = ed + ct;
+				var aSendData = {};
+				aSendData.PATH = Spath;
+				aSendData.DATA = d;
 
-		}
+				oAPP.onCreateBarcodeMatrix.actCnt = 1;
+				oAPP.openModalWindow(loadUrl, sOption, aSendData);
 
+				return;
+			}
+
+
+			var rs = d.length % 3,
+				st = 0,
+				ed = Math.floor(d.length / 3),
+				ct = ed;
+
+			//병렬 수행 카운트 설정 
+			oAPP.onCreateBarcodeMatrix.actCnt = 3;
+
+			for (var i = 1, l = 3; i <= l; i++) {
+
+				//마지막인경우 나머지값을 더해줌.
+				if (i === l) {
+					ed += rs;
+				}
+
+				var aSendData = {};
+				aSendData.PATH = Spath;
+				aSendData.DATA = d.slice(st, ed);
+				oAPP.openModalWindow(loadUrl, sOption, aSendData);
+
+				//호출 이후 exit.
+				if (i === l) {
+					break;
+				}
+
+				//종료값을 시작값으로 옮김.
+				st = ed;
+
+				//종료값 +
+				ed = ed + ct;
+
+			}
+
+		}).catch(function (e) {
+
+			var sMsg = oAPP.onGetMsgTxt("0019"); /* 다운로드 폴더 디렉토리 선택 실패! */
+			alert(sMsg);
+
+			return;
+
+		});
 
 	};
 
